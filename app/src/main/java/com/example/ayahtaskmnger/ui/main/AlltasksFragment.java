74e417303b47fc99d76247ac2991ed31e687fc59
Owner <lama76618@gmail.com>
2019-10-27 12:18:44 +0200

    10-11

--- app/src/main/java/com/example/ayahtaskmnger/ui/main/AlltasksFragment.java
+++ app/src/main/java/com/example/ayahtaskmnger/ui/main/AlltasksFragment.java
@@ -3,17 +3,22 @@ package com.example.ayahtaskmnger.ui.main;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.ayahtaskmnger.R;
+import com.example.ayahtaskmnger.data.MyTask;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -38,6 +43,24 @@ public class AlltasksFragment extends Fragment {
         FirebaseAuth auth=FirebaseAuth.getInstance();//to get correct id
         String uid = auth.getUid();
         DatabaseReference reference = database.getReference();
+
+        reference.child("task").child(uid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //fe al datachange m3alg al7dth
+                for (DataSnapshot d:dataSnapshot.getChildren()) {
+                    MyTask t=d.getValue(MyTask.class);
+                    Log.d("MyTask",t.toString());
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
 
 
     }
